1fd78a6c1384b5eccce1eaca50652bbd531966d6
ShamyLand <147116169+ShamyLand@users.noreply.github.com>
2026-01-22 01:38:29 +0100

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,12 +1,18 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   typescript: {
-    // Force le build même s'il y a des erreurs de dossiers/types
     ignoreBuildErrors: true,
   },
   eslint: {
-    // Ignore les avertissements de code pendant la construction
     ignoreDuringBuilds: true,
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'images.unsplash.com',
+      },
+    ],
   },
 };
 
