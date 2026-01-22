226f95c6298c4fdc04399f0d1f6a0216cad81cb0
ShamyLand <147116169+ShamyLand@users.noreply.github.com>
2026-01-22 01:15:23 +0100

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   typescript: {
+    // Force le build même s'il y a des erreurs de dossiers/types
     ignoreBuildErrors: true,
   },
   eslint: {
+    // Ignore les avertissements de code pendant la construction
     ignoreDuringBuilds: true,
   },
 };
